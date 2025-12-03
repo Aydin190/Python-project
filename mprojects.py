bdb3d31bffb638f08a86b6b59aec5263a1b0fed7
Aydin Nehan <aydinnehan@Aydins-MacBook-Pro.local>
2025-12-03 18:57:19 +0000

    File added

--- mprojects.py
+++ mprojects.py
@@ -7,17 +7,21 @@ score = 0
 
 life = 3
 
+maxRange = 6
+
 for roundNumber in range(1, 11):
     # Check if life is 0, and print, you have 0 lives left. game over. give break statement.
     if life == 0:
         print("You have no lives left! Game Over.")
         break
-    roll = random.randint(1, 6)
+    roll = random.randint(1, maxRange)
     guess = int(input("Guess any number from 1-6"))
 
     if guess == roll:
         print("Your intuition is great.")
         score+=1
+        maxRange+=2
+
     else:
         print(f"Unfortunately, that's the wrong guess. The dice showed {roll}")
         life-=1
@@ -48,3 +52,4 @@ else:
 
 # Adding life systems from the heart mode to this game where you start with 3 lives and your wrong guess will lose 1 of ur lives, and the game ends when you lose all of your lives or you finish all the 10 rounds, the score still counts.
 
+# Increase difficulty with each correct guess. Initially there are 1 to 6 rounds, after that it will be between dice 1-8.
